105ed8f57abdd6d70ab8f74355c236e9f81a1751
JYL35 <dlwodbs1223@naver.com>
2026-01-04 20:19:29 +0900

    feat: 카테고리 추천 기능 구현

--- src/main/java/menu/service/MenuService.java
+++ src/main/java/menu/service/MenuService.java
@@ -1,9 +1,12 @@
 package menu.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import java.util.Map;
+import menu.domain.Category;
 import menu.domain.Coach;
 import menu.repository.CoachRepository;
+import menu.repository.RecommendHistory;
 
 public class MenuService {
 
@@ -12,5 +15,17 @@ public class MenuService {
             List<String> cantEatMenu = allCoachCantEatMenu.get(coachName);
             CoachRepository.addCoach(new Coach(coachName, cantEatMenu));
         }
+    }
+
+    public void startRecommend() {
+        Category category = shuffledCategory();
+    }
+
+    private Category shuffledCategory() {
+        Category category = Category.findCategory(Randoms.pickNumberInRange(1, 5));
+        if (RecommendHistory.hasSelectedCategoryBefore(category)) {
+            return shuffledCategory();
+        }
+        return category;
     }
 }
